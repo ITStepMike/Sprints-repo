7603a3ef74a460d683c041eae2f3ca90351034da
miketi <tmymichael@gmail.com>
2018-11-09 11:37:21 +0200

    shit lbm5

--- go/main.go
+++ go/main.go
@@ -5,27 +5,47 @@ import (
 	"math"
 )
 
+//f(x) = ctg(x) - x/2 = 0
+func f(x float64) float64 {
+
+	return (-1/(math.Sin(x)*math.Sin(x)) - 1/2)
+
+}
+
+//f'(x) = -1/(sin(x)*sin(x)) - 1/2
+func fp1(x float64) float64 {
+
+	return (1/math.Tan(x) - x/2)
+
+}
+
+//f''(x) = (2*cos(x)*cos(x))/sin(x)*sin(x)*sin(x)
+func fp2(x float64) float64 {
+
+	return (2 * math.Cos(x) * math.Cos(x)) / math.Sin(x) * math.Sin(x) * math.Sin(x)
+
+}
 
 func main() {
-  
-  //var xa float64 = 1/math.Tan(x) - x/2
-  var a, acc, b  float64 = 3.5, 0.001, 3.7 // функції будуть перетинатися на графіку (в додатніх випадках) на проміжку від [0;нескінченності)
-  var arr[4] float64
-  j := 0
-  for i := a + acc; j < 4; i+=acc {
+	// var a,b,acc float64 = 1,1.5,0.001
+	var a, b, acc float64 = 1, 1.3, 0.001
+	//var s []float64 //s = append(s,dasdasd)
+	fmt.Println(f(a) * fp2(a)) // f(a) * f''(a) > 0 тоді початкове наближення = 1
+	fmt.Println(f(b) * fp2(b)) // f(a) * f''(a) > 0 тоді початкове наближення = 1
+	var j = 0
+	var xn float64 = a
+	var dx float64
+	var d float64
+	for i := xn; j < 10; i = xn {
 
-    ans := (a - acc) - (1/math.Tan(i) - i/2)/((1/math.Tan(b) - b/2) - (1/math.Tan(i) - i/2)) * (b - (1/math.Tan(i) - i/2))
-    ans = 1/math.Tan(ans) - ans/2
-    fmt.Println(ans)
-    if ans > 0{
-      arr[j] = i
-      arr[j+1] = ans
-      j = 4;
-    }
+		dx := f(i) / fp1(i)
+		fmt.Printf(" %v  x= %f  f(x)= %f  f'(x)= %f  dx= %f \n", j, xn, (1/math.Tan(i) - i/2), -1/(math.Sin(i)*math.Sin(i))-1/2, dx)
+		xn = xn + dx
+		d = math.Abs((xn - dx) - xn)
+		j++
+		fmt.Println(d)
 
-  }
-
-  fmt.Println(arr)
-  
+	}
+	fmt.Println(dx, acc, b)
 
 }
